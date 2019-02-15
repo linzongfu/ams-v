0311fd39434421420a67b9b12956bf697705f60a
linzongfu <2539408294@qq.com>
2019-02-15 12:59:08 +0800

    first

---
+++ src/components/page/Login.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="login-wrap">
+        <div class="ms-login">
+            <div class="ms-title">学生成绩管理系统</div>
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="ms-content">
+                <el-form-item prop="number">
+                    <el-input v-model="ruleForm.number" placeholder="number">
+                        <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input type="password" placeholder="password" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')">
+                        <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
+                    </el-input>
+                </el-form-item>
+                <div class="login-btn">
+                    <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+                </div>
+                <p class="login-tips">Tips : 初始用户名密码是学号</p>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+         data() {
+                return this.tableData.filter((d) => {
+                    let is_del = false;
+                    for (let i = 0; i < this.del_list.length; i++) {
+                        if (d.name === this.del_list[i].name) {
+                            is_del = true;
+                            break;
+                        }
+                    }
+                    if (!is_del) {
+                        if (d.address.indexOf(this.select_cate) > -1 &&
+                            (d.name.indexOf(this.select_word) > -1 ||
+                                d.address.indexOf(this.select_word) > -1)
+                        ) {
+                            return d;
+                        }
+                    }
+                })
+            }
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.$post('/user/login',this.ruleForm)
+                         .then((response) => {
+                             console.log(response)
+                           // console.log(response.token)
+                           if(response.code=="2019200"){
+                             window.localStorage.setItem('token',response.token);
+                             localStorage.setItem('ms_username',this.ruleForm.username);
+                             this.$router.push('/');
+                            }
+                            else{
+                                alert("用户名密码错误")
+                            }
+                        })
+                     
+                        
+                     
+ 
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .login-wrap{
+        position: relative;
+        width:100%;
+        height:100%;
+        background-image: url(../../assets/img/login-bg.jpg);
+        background-size: 100%;
+    }
+    .ms-title{
+        width:100%;
+        line-height: 50px;
+        text-align: center;
+        font-size:20px;
+        color: #fff;
+        border-bottom: 1px solid #ddd;
+    }
+    .ms-login{
+        position: absolute;
+        left:50%;
+        top:50%;
+        width:350px;
+        margin:-190px 0 0 -175px;
+        border-radius: 5px;
+        background: rgba(255,255,255, 0.3);
+        overflow: hidden;
+    }
+    .ms-content{
+        padding: 30px 30px;
+    }
+    .login-btn{
+        text-align: center;
+    }
+    .login-btn button{
+        width:100%;
+        height:36px;
+        margin-bottom: 10px;
+    }
+    .login-tips{
+        font-size:12px;
+        line-height:30px;
+        color:#fff;
+    }
+</style>
